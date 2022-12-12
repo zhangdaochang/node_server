9b4b1340da6b34c9b311f6104cd8091eac6441ed
daochang <2051525775@qq.com>
2022-12-13 03:48:55 +0800

    url parse query Arguments !

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 import * as http from "http";
 import * as fs from "fs";
 import * as p from "path"
+import * as url from "url";
 
 const server = http.createServer()
 const path = p.join(__dirname, "public")
 
 server.on('request', (req, res) => {
-    const {url} = req
-    switch (url) {
+    const {url: u} = req
+    const {pathname,query} = url.parse(u || '')
+    console.log(query);
+    switch (pathname) {
         case '/index.html':
-            fs.readFile(p.join(path, url), (err, data) => {
+            fs.readFile(p.join(path, pathname), (err, data) => {
                 if (err) {
                     throw err
                 }
@@ -18,7 +21,7 @@ server.on('request', (req, res) => {
             break
         case '/main.js':
             res.setHeader('Content-Type', 'text/javascript; charset=utf-8')
-            fs.readFile(p.join(path, url), (err, data) => {
+            fs.readFile(p.join(path, pathname), (err, data) => {
                 if (err) {
                     throw err
                 }
@@ -27,7 +30,7 @@ server.on('request', (req, res) => {
             break
         case '/style.css':
             res.setHeader('Content-Type', 'text/css; charset=utf-8')
-            fs.readFile(p.join(path, url), (err, data) => {
+            fs.readFile(p.join(path, pathname), (err, data) => {
                 if (err) {
                     throw err
                 }
@@ -36,7 +39,7 @@ server.on('request', (req, res) => {
             break
         case '/apple.jpg':
             res.setHeader('Content-Type', 'image/jpeg')
-            fs.readFile(p.join(path, url), (err, data) => {
+            fs.readFile(p.join(path, pathname), (err, data) => {
                 if (err) {
                     throw err
                 }
