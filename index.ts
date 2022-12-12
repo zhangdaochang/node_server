29ac13afd9421f5a2b47271d6e70c7e0d9797889
daochang <2051525775@qq.com>
2022-12-13 04:35:48 +0800

    Auto match file!

--- index.ts
+++ index.ts
@@ -6,46 +6,36 @@ import * as url from "url";
 const server = http.createServer()
 const path = p.join(__dirname, "public")
 
-server.on('request', (req, res) => {
+function matchFile(fileName : string) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(p.join(path, fileName), (err, data) => {
+            if (err) {
+                return reject("找不到啊铁子")
+            }
+            return resolve(data)
+        })
+    })
+}
+
+server.on('request', async (req, res) => {
     const {url: u} = req
-    const {pathname,query} = url.parse(u || '')
-    console.log(query);
-    switch (pathname) {
-        case '/index.html':
-            fs.readFile(p.join(path, pathname), (err, data) => {
-                if (err) {
-                    throw err
-                }
-                res.end(data.toString())
-            })
-            break
-        case '/main.js':
-            res.setHeader('Content-Type', 'text/javascript; charset=utf-8')
-            fs.readFile(p.join(path, pathname), (err, data) => {
-                if (err) {
-                    throw err
-                }
-                res.end(data.toString())
-            })
-            break
-        case '/style.css':
-            res.setHeader('Content-Type', 'text/css; charset=utf-8')
-            fs.readFile(p.join(path, pathname), (err, data) => {
-                if (err) {
-                    throw err
-                }
-                res.end(data.toString())
-            })
-            break
-        case '/apple.jpg':
-            res.setHeader('Content-Type', 'image/jpeg')
-            fs.readFile(p.join(path, pathname), (err, data) => {
-                if (err) {
-                    throw err
-                }
-                res.end(data)
-            })
-            break
+    if(!u)return
+    const {pathname, query} = url.parse(u)
+    const Cotype:any = {
+        "js": "text/javascript; charset=utf-8",
+        "css": "text/css; charset=utf-8",
+        "jpg": "image/jpeg",
+        "html": "text/html; charset=utf-8"
     }
+    if (!pathname) return;
+    let contentType =Cotype[pathname.split('.' )[1]]
+    if(contentType){
+        res.setHeader('Content-Type', contentType)
+    }
+
+    res.end(await matchFile(pathname).catch(async (err)=>{
+        res.statusCode = 404
+        res.end(await matchFile("NotFound.html"))
+    }))
 })
 server.listen(8888)
